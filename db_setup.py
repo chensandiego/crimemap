687b0c24a7a85523252ec96cfc4eb3648ba6cec9
chen <chen@localhost.localdomain>
2016-04-06 16:34:06 +0800

    crime.py

---
+++ db_setup.py
@@ -0,0 +1,15 @@
+import pymysql
+import dbconfig
+connection = pymysql.connect(host='localhost',user=dbconfig.db_user,passwd=dbconfig.db_password)
+
+try:
+	with connection.cursor() as cursor:
+		sql="create database if not exists crimemap"
+		cursor.execute(sql)
+		sql="""create table if not exists crimemap.crimes(id int not null auto_increment,latitude float(10,6),
+			longitude float(10,6),date DATETIME,category varchar(50), description varchar(1000), updated_at timestamp,primary key(id))"""
+		cursor.execute(sql)
+	connection.commit()
+finally:
+	connection.close()
+	
