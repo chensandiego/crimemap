551e61c556009d41d160c64d718d6630be4c8b70
chen <chen@localhost.localdomain>
2016-04-07 12:58:30 +0800

    add validation and sanitization

--- crimemap.py
+++ crimemap.py
@@ -5,6 +5,7 @@ import json
 import dbconfig
 import datetime
 import dateparser
+import string
 from dbhelper import DBHelper
 
 app = Flask(__name__)
@@ -13,10 +14,11 @@ DB = DBHelper()
 categories=['mugging','break-in']
 
 @app.route("/")
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes,categories=categories)
+
+    return render_template("home.html", crimes=crimes,categories=categories,error_message=error_message)
 	#return render_template("home.html")
 
 
@@ -26,15 +28,21 @@ def submitcrime():
     if category not in categories:
     	return home()
     date = request.form.get("date")
+    if not date:
+        return home("Invalid date. Please use yyyy-mm-dd format")
     try:
     	latitude = float(request.form.get("latitude"))
     	longitude = float(request.form.get("longitude"))
     except ValueError:
     	return home()
     description = request.form.get("description")
+    description=sanitize_string(request.form.get("description"))
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
 
+def sanitize_string(userinput):
+    whitelist = string.letters + string.digits + "!?$.,;:-'()&"
+    return filter(lambda x" x in whitelist,userinput)
 
 def format_date(userdate):
 	date = dateparser.parse(userdate)
